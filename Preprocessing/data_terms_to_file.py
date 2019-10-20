21f3fa766e409b819afb04ef08051d363dbd93a1
kewilliams86 <kewilliams86@gmail.com>
2019-10-20 14:17:51 -0400

    add sample of only 100 pmids, updated preprocessing code

--- Preprocessing/data_terms_to_file.py
+++ Preprocessing/data_terms_to_file.py
@@ -30,11 +30,16 @@ with open(termFile) as inFile:
             pmidTermDict[data[0]].append(data[1])
 
 
+#count = 0
+
 with open(outFile, 'w') as writeFile:
     with open(dataFile) as inFile:
         for line in inFile:
             data = splitLine(line)
             termList = pmidTermDict[eval(data[0])]
             terms = ('\t').join(termList)
-            
             writeFile.write(eval(data[0]) + '\t' + eval(data[1]) + '\t' + eval(data[5]) + '\t' + terms + '\n')
+#            count += 1
+#            if count >= 100:
+#                break
+            
