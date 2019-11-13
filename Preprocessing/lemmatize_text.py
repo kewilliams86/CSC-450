c49ef8fdebd2148149f8776bed6d22c03912bccf
kewilliams86 <kewilliams86@gmail.com>
2019-11-13 10:43:31 -0500

    lemmatize text and remove stopwords, punctuation, numbers, words > len 2

---
+++ Preprocessing/lemmatize_text.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 16:07:33 2019
+
+@author: kewilliams
+
+Partial code adapted from the response by user 'slider' on stackoverflow
+https://stackoverflow.com/questions/37605710/tokenize-a-paragraph-into-sentence-and-then-into-words-in-nltk
+"""
+
+import re
+from nltk.corpus import wordnet as wn
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk import word_tokenize, pos_tag
+from collections import defaultdict
+from nltk.corpus import stopwords
+import nltk
+import timeit
+
+stop_word = stopwords.words('english')
+stop_word_set = set(stopwords.words('english')) # curious for speed testing, O(1) instead of linear
+
+lemma_function = WordNetLemmatizer()
+
+tag_map = defaultdict(lambda : wn.NOUN)
+tag_map['J'] = wn.ADJ
+tag_map['V'] = wn.VERB
+tag_map['R'] = wn.ADV
+
+def lemmatizeText(text):
+    
+    #unsure of how tokenization parses ' and "
+#    pattern = re.compile("\'|\"|\-") # remove -, ', " before lemmatization
+#    text = pattern.sub('', text)
+#    text = text.replace('-', '') # only remove '-', want to reduce word with -
+
+    sentence = [] # if for loop is skipped need empty list
+    sent_text = nltk.sent_tokenize(data[0])
+    for s in sent_text:
+        sentence = []
+        tokens = word_tokenize(s)
+        for token, tag in pos_tag(tokens):
+            lemma = lemma_function.lemmatize(token, tag_map[tag[0]])
+            if lemma.isalnum() and not lemma.isdigit() and len(lemma) > 2 and lemma not in stop_word_set:
+                sentence.append(lemma.lower())
+    return (' ').join(sentence)
+
+directory = '/home/kewilliams/Documents/CSC-450/preprocessed_data/'
+
+articleCount = 0
+t0 = timeit.default_timer()
+
+print('writing to file...')
+with open(directory + 'all_file_terms_lemmatize.txt', 'w') as writeFile:
+    with open(directory + 'all_articles_no_mod.txt') as inFile:
+        for line in inFile:
+            articleCount += 1
+            data = line.strip('\n').split('\t')
+            writeFile.write(lemmatizeText(data[0]) + '\t' + lemmatizeText(data[1]) + '\t' + data[2] + '\n')
+            if articleCount % 20000 == 0:
+                t1 = timeit.default_timer()
+                print(str(articleCount) + ' articles written... ' + str(t1 - t0) + ' seconds')
+                t0 = timeit.default_timer()
